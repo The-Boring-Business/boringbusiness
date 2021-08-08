059334d1b1e7c7a1a764d16be4fc034b47148f01
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-08-08 01:17:56 -0400

    fixed up create post input

--- src/components/Schedule Post/CreatePost.js
+++ src/components/Schedule Post/CreatePost.js
@@ -1,31 +1,63 @@
 import PostInput from "./subcomponents/PostInput";
 import PostToggle from "./subcomponents/PostToggle";
-import DOB from "./subcomponents/DOB"
+import DOB from "./subcomponents/DOB";
+import Icon from "./../UI Elements/Icon";
+import gallery from "./../../style/Icons/SchedulePost/gallery.svg";
+import wand from "./../../style/Icons/SchedulePost/wand.png";
+import checkActive from "./../../style/Icons/SchedulePost/check_active.svg";
+import checkUnactive from "./../../style/Icons/SchedulePost/check_unactive.svg";
+
 
 const CreatePost = () => {
+  const handleSubmit = (event) => {};
+
   return (
     <div className="component create-post">
-      <h2>Create New Post</h2>
-      <PostInput
-        title="Caption"
-        placeholder="Type something..."
-        imageUpload="True"
-        magicWand="False"
-      />
-      <PostInput
-        title="Hashtags"
-        placeholder="Add your own..."
-        imageUpload="False"
-        magicWand="True"
-      />
-      <DOB
-        title="When to Post"
-      />
-      <PostToggle />
-      <div class="create-post-btn-container">
-        {/* <button className="btn-primary">Post Now</button> */}
-        <button className="btn-primary">Save & Schedule</button>
-      </div>
+      <form>
+        <h2>Create New Post</h2>
+        <div className="post-input-component">
+          <p>Enter Caption</p>
+          <div className="input-text">
+            <input name="caption" type="text" placeholder="Post Caption" />
+            <div className="post-input-icons">
+              <Icon src={gallery} height="30px" />
+              <Icon src={wand} height="30px" />
+            </div>
+          </div>
+        </div>
+        <div className="post-input-component">
+          <p>Enter Caption</p>
+          <div className="input-text">
+            <input name="hashtags" type="text" placeholder="Hashtags" />
+            <div className="post-input-icons">
+              <Icon src={wand} height="30px" />
+            </div>
+          </div>
+        </div>
+        <div className="profile-input">
+          <p style={{ fontWeight: "700" }}>Date</p>
+          <div className="input-text">
+            <input name="date" type="date" />
+          </div>
+        </div>
+        <div className="post-toggle">
+          <p>Where to Post</p>
+          <div className="post-toggle-btn-container">
+            <button className="btn-primary post-toggle-btn">
+              <Icon src={checkActive} height="40px" />
+              Instagram
+            </button>
+            <button className="btn-primary post-toggle-btn">
+              <Icon src={checkUnactive} height="40px" />
+              Twitter
+            </button>
+          </div>
+        </div>
+        <div class="create-post-btn-container">
+          {/* <button className="btn-primary">Post Now</button> */}
+          <button className="btn-primary">Save & Schedule</button>
+        </div>
+      </form>
     </div>
   );
 };
